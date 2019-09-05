05869188beef219abfd28b454660e8ef9a5fe9f8
Атанас Станчев <mr_Stanchev@hotmail.com>
2019-09-06 00:48:46 +0300

    Choosing letter from Bulgarian alphabet. Created with windows forms.

---
+++ Draw Letter/Draw Letter/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Draw_Letter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonRun.Location = new System.Drawing.Point(142, 130);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(149, 52);
+            this.buttonRun.TabIndex = 0;
+            this.buttonRun.Text = "Start";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label.Location = new System.Drawing.Point(177, 21);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(83, 91);
+            this.label.TabIndex = 1;
+            this.label.Text = "?";
+            this.label.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 194);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.buttonRun);
+            this.Name = "Form1";
+            this.Text = "Избор на буква";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Draw Letter/Draw Letter/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Draw_Letter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int stopAndStart = 0;
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            if (stopAndStart == 0)
+            {
+                timer1.Enabled = true;
+                stopAndStart = 1;
+                buttonRun.Text = "Stop";
+            }
+            else
+            {
+                timer1.Enabled = false;
+                stopAndStart = 0;
+                buttonRun.Text = "Start";
+            }
+        }
+
+        private char[] cirilicLetters =
+        {
+            'А', 'Б', 'В', 'Г', 'Д',
+            'Е', 'Ж', 'З', 'И', 'Й',
+            'К', 'Л', 'М', 'Н', 'О',
+            'П', 'Р', 'С', 'Т', 'У',
+            'Ф', 'Х', 'Ц', 'Ч', 'Ъ',
+            'ь', 'Ю', 'Я'
+        };
+        private int increment = 0;
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            increment++;
+            label.Text = cirilicLetters[increment].ToString();
+            if (increment == cirilicLetters.Length - 1)
+            {
+                increment = 0;
+            }
+        }
+
+        private void Label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
